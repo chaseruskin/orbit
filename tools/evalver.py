f728bd3895b1c589b07ec9685851944340f95295
Chase Ruskin <c.ruskin@ufl.edu>
2022-03-13 15:26:18 -0400

    ci: adds evalver script for check-release action

---
+++ tools/evalver.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------------------------
+# File     : evalver.py
+# Author   : Chase Ruskin
+# Abstract :
+#   Evaluate the version in the Cargo.toml manifest with the latest version of
+#   this branch. A '1' will indicate the requested manifest version is larger
+#   than the previously tagged version. A '0' indicates otherwise.
+# ------------------------------------------------------------------------------
+
+print('0')

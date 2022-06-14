834bd45ab28af59c4263feb72897d1c356177dcd
Chase Ruskin <c.ruskin@ufl.edu>
2022-06-14 10:01:46 -0400

    fix: windows-style endings supported when given prompts

--- src/util/prompt.rs
+++ src/util/prompt.rs
@@ -6,15 +6,17 @@ pub fn prompt(s: &str) -> Result<bool, Error> {
     check_for_response(&mut io::stdin().lock())
 }
 
-/// Forever loops until a valid response is entered. "Y\n" and "\n" map to `true`, while
+/// Infinitely loops until a valid response is entered. "Y\n" and "\n" map to `true`, while
 /// "N\n" maps to `false`.
+/// 
+/// Also supports checking windows-style line endings `\r\n`.
 fn check_for_response(input: &mut (impl Read + std::io::BufRead)) -> Result<bool, Error> {
     let mut buffer: String = String::new();
     loop {
         input.read_line(&mut buffer)?;
         let result = match buffer.to_uppercase().as_ref() {
-            "\n" | "Y\n" => Some(true),
-            "N\n" => Some(false),
+            "\r\n" | "\n" | "Y\n" | "Y\r\n" => Some(true),
+            "N\n" | "N\r\n" => Some(false),
             _ => {
                 buffer.clear();
                 None
@@ -43,5 +45,15 @@ mod test {
         assert_eq!(r, true);
         let r = check_for_response(&mut "y\n".as_bytes()).unwrap();
         assert_eq!(r, true);
+    }
+
+    #[test]
+    fn windows_style() {
+        let r = check_for_response(&mut "y\r\n".as_bytes()).unwrap();
+        assert_eq!(r, true);
+        let r = check_for_response(&mut "\r\n".as_bytes()).unwrap();
+        assert_eq!(r, true);
+        let r = check_for_response(&mut "N\r\n".as_bytes()).unwrap();
+        assert_eq!(r, false);
     }
 }

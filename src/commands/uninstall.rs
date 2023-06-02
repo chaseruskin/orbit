7a194d7b36d980f05335e37c629cba0311d9b3c4
Chase Ruskin <c.ruskin@ufl.edu>
2023-06-01 20:28:25 -0600

    allows uninstall to specify version through ip spec arg

--- src/commands/uninstall.rs
+++ src/commands/uninstall.rs
@@ -1,9 +1,8 @@
 use clif::cmd::{FromCli, Command};
 use crate::core::v2::catalog::Catalog;
-use crate::core::pkgid::PkgPart;
-use crate::core::version::AnyVersion;
+use crate::core::v2::ip::PartialIpSpec;
 use clif::Cli;
-use clif::arg::{Positional, Optional};
+use clif::arg::{Positional, Flag};
 use clif::Error as CliError;
 use crate::core::context::Context;
 use crate::util::anyerror::AnyError;
@@ -12,17 +11,18 @@ use std::fs;
 
 #[derive(Debug, PartialEq)]
 pub struct Uninstall {
-    ip: PkgPart,
-    version: AnyVersion
-    // @TODO add option to remove all versions (including store)
+    ip: PartialIpSpec,
+    full: bool,
+    // @todo: add option to remove all versions (including store)
+    // @todo: 
 }
 
 impl FromCli for Uninstall {
     fn from_cli<'c>(cli: &'c mut Cli) -> Result<Self,  CliError> {
         cli.check_help(clif::Help::new().quick_text(HELP).ref_usage(2..4))?;
         let command = Ok(Uninstall {
-            ip: cli.require_positional(Positional::new("ip"))?,
-            version: cli.check_option(Optional::new("ver").switch('v').value("version"))?.unwrap_or(AnyVersion::Latest),
+            full: cli.check_flag(Flag::new("full"))?,
+            ip: cli.require_positional(Positional::new("spec"))?,
         });
         command
     }
@@ -43,7 +43,7 @@ impl Command<Context> for Uninstall {
         // let target = crate::core::ip::find_ip(&self.ip, ids)?;
 
         // check for ip in development or installation
-        let status = match catalog.inner().get(&self.ip) {
+        let status = match catalog.inner().get(&self.ip.get_name()) {
             Some(st) => st,
             None => {
                 return Err(AnyError(format!("ip '{}' does not exist in the cache", self.ip)))?
@@ -51,9 +51,9 @@ impl Command<Context> for Uninstall {
         };
 
         // grab the ip's manifest
-        let target = match status.get_install(&self.version) {
+        let target = match status.get_install(&self.ip.get_version()) {
                 Some(t) => t,
-                None => return Err(AnyError(format!("ip '{}' is not installed to the cache under version '{}'", self.ip, self.version)))?
+                None => return Err(AnyError(format!("IP {} does not exist in the cache", self.ip)))?
         };
 
         // delete the project
@@ -79,8 +79,8 @@ Usage:
     orbit uninstall [options] <ip>
 
 Args:
-    <ip>                    the name corresponding to the ip to delete
-    --ver, -v <version>     the version of the IP to remove
+    <spec>      the name corresponding to the ip to delete
+    --full      fully remove the ip and its dependencies
 
 Use 'orbit help uninstall' to learn more about the command.
 ";

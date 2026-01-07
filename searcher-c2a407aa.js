500c1bb0a9c4a95720a9441abed8c220abd2aaf1
chaseruskin <chaseruskin@users.noreply.github.com>
2026-01-07 02:41:49 +0000

    Updates license header aebee0df47a5a3ed6ce6d62bea6bb393e8938f48

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-09b9e77b.js',
+                path_to_root + 'searchindex-6e9ba2a6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

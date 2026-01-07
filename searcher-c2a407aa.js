b2246eac8555ea069b4f8bc034c0bd2ab5485b37
chaseruskin <chaseruskin@users.noreply.github.com>
2026-01-07 03:27:39 +0000

    Fixes bug regarding lockfile synchronization during a local project install 2b4d1cb415e1ea5d1e2fd5beb6e1eb93bcf8a2e6

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6e9ba2a6.js',
+                path_to_root + 'searchindex-bcc65cca.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

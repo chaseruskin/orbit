4bfea7a1125d25c1b81d2cf94b1ac54c8b1aa501
chaseruskin <chaseruskin@users.noreply.github.com>
2026-03-22 04:33:11 +0000

    Stages for release d871c4eb7e197b4458a8f7fc75d19c5a95a0d201

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2c25a4a8.js',
+                path_to_root + 'searchindex-32a34ee7.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

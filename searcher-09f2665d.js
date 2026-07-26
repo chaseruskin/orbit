5e429363b4775c3eb897233d5efdb02b71823e88
chaseruskin <chaseruskin@users.noreply.github.com>
2026-07-26 18:18:20 +0000

    Updates transitive dependencies 7082655814da1917fd6f0b43326854e18f554771

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-32a34ee7.js',
+                path_to_root + 'searchindex-97abda36.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
